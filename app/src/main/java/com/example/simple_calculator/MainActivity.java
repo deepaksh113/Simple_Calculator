25831261bdbcb9f4e492857524b89b9b64b66d64
Deepak <deepak.sharma4205@gmail.com>
2020-11-02 18:26:04 +0530

    warnings removed

--- app/src/main/java/com/example/simple_calculator/MainActivity.java
+++ app/src/main/java/com/example/simple_calculator/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.simple_calculator;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
@@ -36,189 +35,115 @@ public class MainActivity extends AppCompatActivity {
 
         editText = (TextView) findViewById(R.id.display);
 
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                editText.setText(format("%s1", editText.getText()));
+        button1.setOnClickListener(v -> editText.setText(format("%s1", editText.getText())));
+
+        button2.setOnClickListener(v -> editText.setText(format("%s2", editText.getText())));
+
+        button3.setOnClickListener(v -> editText.setText(format("%s3", editText.getText())));
+
+        button4.setOnClickListener(v -> editText.setText(format("%s4", editText.getText())));
+
+        button5.setOnClickListener(v -> editText.setText(format("%s5", editText.getText())));
+
+        button6.setOnClickListener(v -> editText.setText(format("%s6", editText.getText())));
+
+        button7.setOnClickListener(v -> editText.setText(format("%s7", editText.getText())));
+
+        button8.setOnClickListener(v -> editText.setText(format("%s8", editText.getText())));
+
+        button9.setOnClickListener(v -> editText.setText(format("%s9", editText.getText())));
+
+        button0.setOnClickListener(v -> editText.setText(format("%s0", editText.getText())));
+
+        buttonAddition.setOnClickListener(v -> {
+            if (editText.getText().length() != 0) {
+                input1 = Float.parseFloat(editText.getText() + "");
+                addition = true;
+                decimal = false;
+                editText.setText(null);
             }
         });
 
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                editText.setText(format("%s2", editText.getText()));
+        buttonSubtraction.setOnClickListener(v -> {
+            if (editText.getText().length() != 0) {
+                input1 = Float.parseFloat(editText.getText() + "");
+                subtract = true;
+                decimal = false;
+                editText.setText(null);
             }
         });
 
-        button3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                editText.setText(format("%s3", editText.getText()));
+        buttonMultiplication.setOnClickListener(v -> {
+            if (editText.getText().length() != 0) {
+                input1 = Float.parseFloat(editText.getText() + "");
+                multiplication = true;
+                decimal = false;
+                editText.setText(null);
             }
         });
 
-        button4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                editText.setText(format("%s4", editText.getText()));
+        buttonDivision.setOnClickListener(v -> {
+            if (editText.getText().length() != 0) {
+                input1 = Float.parseFloat(format("%s", editText.getText()));
+                division = true;
+                decimal = false;
+                editText.setText(null);
             }
         });
 
-        button5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                editText.setText(format("%s5", editText.getText()));
+        remainder1.setOnClickListener(v -> {
+            if (editText.getText().length() != 0) {
+                input1 = Float.parseFloat(format("%s", editText.getText()));
+                remainder = true;
+                decimal = false;
+                editText.setText(null);
             }
         });
 
-        button6.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                editText.setText(format("%s6", editText.getText()));
+        buttonEqual.setOnClickListener(v -> {
+            if (addition || subtract || multiplication || division || remainder) {
+                input2 = Float.parseFloat(String.format("%s", editText.getText()));
+            }
+
+            if (addition) {
+
+                editText.setText(format("%s", input1 + input2));
+                addition = false;
+            }
+
+            if (subtract) {
+
+                editText.setText(format("%s", input1 - input2));
+                subtract = false;
+            }
+
+            if (multiplication) {
+                editText.setText(format("%s", input1 * input2));
+                multiplication = false;
+            }
+
+            if (division) {
+                editText.setText(format("%s", input1 / input2));
+                division = false;
+            }
+            if (remainder) {
+                editText.setText(format("%s", input1 % input2));
+                remainder = false;
             }
         });
 
-        button7.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                editText.setText(format("%s7", editText.getText()));
-            }
+        buttonDeletion.setOnClickListener(v -> {
+            editText.setText("");
+            input1 = 0.0;
+            input2 = 0.0;
         });
 
-        button8.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                editText.setText(format("%s8", editText.getText()));
+        buttonDot.setOnClickListener(v -> {
+            if (!decimal) {
+                editText.setText(format("%s.", editText.getText()));
+                decimal = true;
             }
-        });
 
-        button9.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                editText.setText(format("%s9", editText.getText()));
-            }
-        });
-
-        button0.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                editText.setText(format("%s0", editText.getText()));
-            }
-        });
-
-        buttonAddition.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (editText.getText().length() != 0) {
-                    input1 = Float.parseFloat(editText.getText() + "");
-                    addition = true;
-                    decimal = false;
-                    editText.setText(null);
-                }
-            }
-        });
-
-        buttonSubtraction.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (editText.getText().length() != 0) {
-                    input1 = Float.parseFloat(editText.getText() + "");
-                    subtract = true;
-                    decimal = false;
-                    editText.setText(null);
-                }
-            }
-        });
-
-        buttonMultiplication.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (editText.getText().length() != 0) {
-                    input1 = Float.parseFloat(editText.getText() + "");
-                    multiplication = true;
-                    decimal = false;
-                    editText.setText(null);
-                }
-            }
-        });
-
-        buttonDivision.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (editText.getText().length() != 0) {
-                    input1 = Float.parseFloat(format("%s", editText.getText()));
-                    division = true;
-                    decimal = false;
-                    editText.setText(null);
-                }
-            }
-        });
-
-        remainder1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (editText.getText().length() != 0) {
-                    input1 = Float.parseFloat(format("%s", editText.getText()));
-                    remainder = true;
-                    decimal = false;
-                    editText.setText(null);
-                }
-            }
-        });
-
-        buttonEqual.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (addition || subtract || multiplication || division || remainder) {
-                    input2 = Float.parseFloat(String.format("%s", editText.getText()));
-                }
-
-                if (addition) {
-
-                    editText.setText(format("%s", input1 + input2));
-                    addition = false;
-                }
-
-                if (subtract) {
-
-                    editText.setText(format("%s", input1 - input2));
-                    subtract = false;
-                }
-
-                if (multiplication) {
-                    editText.setText(format("%s", input1 * input2));
-                    multiplication = false;
-                }
-
-                if (division) {
-                    editText.setText(format("%s", input1 / input2));
-                    division = false;
-                }
-                if (remainder) {
-                    editText.setText(format("%s", input1 % input2));
-                    remainder = false;
-                }
-            }
-        });
-
-        buttonDeletion.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                editText.setText("");
-                input1 = 0.0;
-                input2 = 0.0;
-            }
-        });
-
-        buttonDot.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (!decimal) {
-                    editText.setText(format("%s.", editText.getText()));
-                    decimal = true;
-                }
-
-            }
         });
 
     }
